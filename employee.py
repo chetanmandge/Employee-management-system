e3e68dbe7d41ccc86e928e3e73249c9cd591621e
csm.rd <chetanmandge05@gmail.com>
2022-01-24 16:31:02 +0530

    add new line

--- employee.py
+++ employee.py
@@ -13,4 +13,34 @@ def menu():
     ''')
     print("="*50)
 
-menu()
+def add_employee():
+    pass
+
+def update():
+    pass
+
+def remove():
+    pass
+
+def show_all():
+    pass
+
+while True:
+    menu()
+    Get_ip=input("Enter your choice you want to perform :-")
+
+    if(Get_ip=='1'):
+        add_employee()
+    elif(Get_ip=='2'):
+        update()
+    elif(Get_ip=='3'):
+        remove()
+    elif(Get_ip=='4'):
+        show_all()
+    else:
+        print("You have enter invalid input....")
+
+    get_ch=input('Do you want to continue Y/N:-')
+    get_ch= get_ch.lower()
+    if(get_ch!= 'y'):
+        break
