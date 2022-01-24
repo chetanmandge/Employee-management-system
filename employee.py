e4f357c3e900a4faa93ac84ccc2c54d3ba6e8ff4
csm.rd <chetanmandge05@gmail.com>
2022-01-24 16:23:58 +0530

    add new line

--- employee.py
+++ employee.py
@@ -1,3 +1,16 @@
-print("="*50)
-print("welcome to employee management system")
-print("="*50)
+
+
+def menu():
+    print("="*50)
+    print("welcome to employee management system")
+    print('''
+                    || MENU ||
+                    1. Add employee.
+                    2. Update employee.
+                    3. Remove.
+                    4. Show all.    
+    
+    ''')
+    print("="*50)
+
+menu()
